c4879286791a103241bd8f1e88440a9d52dea911
jingkui.wang <jingkui.wang@imp.ac.at>
2019-05-09 11:32:24 +0200

    update scripts/scRNAseq_functions.R

--- scripts/scRNAseq_functions.R
+++ scripts/scRNAseq_functions.R
@@ -90,6 +90,220 @@ aggrate.nf.QCs = function(dirs.all, modules2cat = c("star", "featureCounts"))
 }
 
 
+##########################################
+# function for technical replicates 
+##########################################
+compare.techinical.replicates = function(design, counts)
+{
+  library(SingleCellExperiment)
+  library(scater)
+  options(stringsAsFactors = FALSE)
+  
+  # change seqInfos in design to label technicalreps 
+  design$seqInfos[which(design$seqInfos=="R7130_HLWTCBGX9_1")] = "R7130_HHG5KBGX9_1_techrep_nexseq"
+  design$seqInfos[which(design$seqInfos=="R7130_CCYTEANXX_4")] = "R7130_HHGHNBGX9_1_techrep_hiseq"
+  design$seqInfos[which(design$seqInfos=="R7133_CD2GTANXX_5")] = "R7130_HHGHNBGX9_1_techrep_hiseq_R7133"
+  
+  ## add some new features for design for quality controls
+  design$log10_Total = log10(design$total_reads)
+  #design$percent_mapped = design$uniquely_mapped/design$T
+  #design$percent_assigned = design$Assigned/design$uniquely_mapped
+  design$percent_rRNA = design$rRNA / design$Total
+  
+  ## make SCE object and remove genes with zero reads detected
+  sce <- SingleCellExperiment(assays = list(counts = counts), 
+                              colData = as.data.frame(design), 
+                              rowData = data.frame(gene_names = rownames(counts), feature_symbol = rownames(counts)))
+  #write.csv(counts(sce), file=paste0(tabDir, "scRNAseq_raw_readCounts", version.analysis, ".csv"), row.names=TRUE)
+  keep_feature <- rowSums(counts(sce) > 0) > 0
+  sce <- sce[keep_feature, ]
+  
+  
+  #is.spike <- grepl("^ERCC", rownames(sce))
+  is.mito <- rownames(sce) %in% gg.Mt;
+  is.ribo <- rownames(sce) %in% gg.ribo;
+  summary(is.mito)
+  summary(is.ribo)
+  
+  sce <- calculateQCMetrics(sce, feature_controls=list(Mt=is.mito, Ribo=is.ribo))
+  
+  head(colnames(colData(sce)), 20)
+  
+  # some general statistics for each request and lane
+  #cols = c(rep('gray', 2), 'red', 'darkblue', 'darkred', 'blue', 'red')
+  plotColData(sce, y = "log10_Total", x = "seqInfos") + ggtitle("total nb of reads")
+  plotColData(sce, y="uniquely_mapped_percent", x="seqInfos") + ggtitle("% of uniquely mapped ")
+  plotColData(sce, y="percent_assigned", x="seqInfos") + ggtitle("% of assigned")
+  plotColData(sce, y="pct_counts_Ribo", x="seqInfos") + ggtitle("% of rRNA contamination")
+  plotColData(sce, y="pct_counts_Mt", x="seqInfos") + ggtitle("% of Mt")
+  
+  plotColData(sce, y="log10_total_counts", x="seqInfos") + ggtitle("total nb of reads mapped to transcripts")
+  plotColData(sce, y="total_features_by_counts", x="seqInfos") + ggtitle("total nb of genes")
+  
+  plotColData(sce, 
+              x = "log10_Total",
+              y = "uniquely_mapped_percent",
+              #colour_by = "uniquely_mapped_percent",
+              colour_by = "seqInfos",
+              size_by = "pct_counts_Ribo"
+  )
+  
+  plotColData(sce,
+              x = "log10_total_counts",
+              y = "log10_total_features_by_counts",
+              #colour_by = "percent_mapped",
+              colour_by = "seqInfos",
+              size_by = "pct_counts_Mt"
+  ) + scale_x_continuous(limits=c(4, 7)) +
+    scale_y_continuous(limits = c(2.5, 4.1)) +
+    geom_hline(yintercept=log10(c(500, 1000, 5000)) , linetype="dashed", color = "darkgray", size=0.5) +
+    geom_vline(xintercept = c(4:6), linetype="dotted", color = "black", size=0.5)
+  
+  # filter cell and genes
+  threshod.total.counts.per.cell = 10^4
+  threshod.nb.detected.genes.per.cell = 1000;
+  
+  filter_by_total_counts <- (sce$total_counts > threshod.total.counts.per.cell)
+  table(filter_by_total_counts)
+  filter_by_expr_features <- (sce$total_features_by_counts > threshod.nb.detected.genes.per.cell)
+  table(filter_by_expr_features)
+  filter_by_MT = sce$pct_counts_Mt < 7.5
+  table(filter_by_MT)
+  
+  sce$use <- (
+    filter_by_expr_features & # sufficient features (genes)
+      filter_by_total_counts & # sufficient molecules counted
+      # filter_by_ERCC & # sufficient endogenous RNA
+      filter_by_MT # remove cells with unusual number of reads in MT genes
+  )
+  table(sce$use)
+  sce = sce[, sce$use]
+  
+  num.cells <- nexprs(sce, byrow=TRUE)
+  ave.counts <- calcAverage(sce)
+  genes.to.keep <- num.cells > 5 & ave.counts >= 1  & ave.counts <10^6  # detected in >= 2 cells, ave.counts >=5 but not too high
+  summary(genes.to.keep)
+  # remove mt and ribo genes
+  genes.to.keep = genes.to.keep & ! rownames(sce) %in% gg.Mt & ! rownames(sce) %in% gg.ribo
+  summary(genes.to.keep)
+  
+  sce <- sce[genes.to.keep, ]
+  
+  
+  # select cells having technical replicates and normalize them  
+  library(scRNA.seq.funcs)
+  library(scater)
+  library(scran)
+  options(stringsAsFactors = FALSE)
+  
+  sce$sels = (sce$request !="R6875" & sce$request != "R7116" & sce$seqInfos != "R7130_HHG5KBGX9_1_techrep_nexseq" &
+                sce$seqInfos != "R7130_HHG5KBGX9_1")
+  sce.qc = sce[, sce$sels]
+  
+  reducedDim(sce.qc) <- NULL
+  endog_genes <- !rowData(sce.qc)$is_feature_control
+  
+  set.seed(1234567)
+  assay(sce.qc, "logcounts") <- log2(calculateCPM(sce.qc, use_size_factors = FALSE) + 1)
+  #sizeFactors(sce.qc) = calculate.sizeFactors.DESeq2(counts(sce.qc))
+  #sce.qc <- normalize(sce.qc, exprs_values = "counts", return_log = TRUE)
+  #qclust <- quickCluster(sce.qc, min.size = 30)
+  #sce.qc <- computeSumFactors(sce.qc, sizes = 15, clusters = qclust)
+  #sce.qc <- normalize(sce.qc, exprs_values = "counts", return_log = TRUE)
+  
+  main = "cpm"
+  scater::plotPCA(
+    sce.qc[endog_genes, ],
+    run_args = list(exprs_values = "logcounts"), 
+    size_by = "total_counts",
+    #size_by = "total_features_by_counts",
+    colour_by = "seqInfos"
+  ) + ggtitle(paste0("PCA -- ", main))
+  
+  set.seed(1234567)
+  param.perplexity = 10;
+  plotTSNE(
+    sce.qc[endog_genes, ],
+    run_args = list(exprs_values = "logcounts", perplexity = param.perplexity), 
+    size_by = "total_counts",
+    #size_by = "total_features_by_counts",
+    colour_by = "seqInfos"  
+  ) + ggtitle(paste0("tSNE - perplexity = ", param.perplexity, "--", main))
+  
+  plotUMAP(
+    sce.qc[endog_genes, ],
+    run_args = list(exprs_values = "logcounts"), 
+    size_by = "total_counts",
+    #size_by = "total_features_by_counts",
+    colour_by = "seqInfos"
+  ) + ggtitle(paste0("UMAP -- ", main))
+  
+  ## check the correction of the same cells from different technical replicates
+  bcs = unique(sce.qc$barcodes)
+  correlations = c()
+  for(n in 1:length(bcs))
+  {
+    #n = 1
+    xx = as.data.frame(logcounts(sce.qc[, which(sce.qc$barcodes == bcs[n])]))
+    if(ncol(xx) == 3) correlations = rbind(correlations, c(cor(xx[, 1], xx[, 2]), cor(xx[, 1], xx[, 3]), cor(xx[, 2], xx[, 3])))
+  }
+  
+  colnames(correlations) = c('rep0.vs.hiseq.rep1', 'rep0.vs.hiseq.rep2', 'hiseq.rep1.vs.hiseq.rep_2')
+}
+
+panel.cor <- function(x, y, digits=2, prefix="", cex.cor) 
+{
+  usr <- par("usr"); on.exit(par(usr)) 
+  par(usr = c(0, 1, 0, 1)) 
+  r <- abs(cor(x, y)) 
+  txt <- format(c(r, 0.123456789), digits=digits)[1] 
+  txt <- paste(prefix, txt, sep="") 
+  if(missing(cex.cor)) cex <- 0.8/strwidth(txt) 
+  
+  test <- cor.test(x,y) 
+  # borrowed from printCoefmat
+  Signif <- symnum(test$p.value, corr = FALSE, na = FALSE, 
+                   cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                   symbols = c("***", "**", "*", ".", " ")) 
+  
+  text(0.5, 0.5, txt, cex = cex * r) 
+  text(.8, .8, Signif, cex=cex, col=2) 
+}
+
+panel.fitting = function (x, y, bg = NA, pch = par("pch"), cex = 0.2, col='black') 
+{
+  #x = yy[,1];y=yy[,2];
+  #kk = which(x>0 & y>0); x=x[kk];y=y[kk]
+  lims = range(c(x, y), na.rm = TRUE)
+  points(x, y, pch = 1, col = col, cex = cex, xlim=lims, ylim=lims)
+  abline(0, 1, lwd=1.5, col='red')
+  R = cor(x, y, use="na.or.complete", method='pearson')
+  text(lims[2]*0.2, lims[2]*0.9, paste0('R = ', signif(R, d=2)), cex=1., col='red')
+  #jj = which(!is.na(x) & !is.na(y))
+  #fit = lm(y[jj] ~ x[jj])
+  #slope=summary(fit)$coefficients[1]
+  #slope = fit$coefficients[2]
+  #intercept = fit$coefficients[1]
+  #pval=summary(fit)$coefficients[4]
+  #abline(intercept, slope, lwd=1.2, col='darkblue', lty=3)
+  #text(lims[2]*0.1, lims[2]*0.7, paste0('slop = ', signif(slope, d=2)), cex=1., col='blue')
+  #text(lims[2]*0.1, lims[2]*0.6, paste0('pval = ', signif(pval, d=2)), cex=1., col='blue')
+  #ok <- is.finite(x) & is.finite(y)
+  #if (any(ok)) 
+  #lines(stats::lowess(x[ok], y[ok], f = span, iter = iter), 
+  #        col = col.smooth, ...)
+}
+
+plot.pair.comparison.plot = function(xx, linear.scale = TRUE, main = "pairwise.comparision"){
+  yy = as.matrix(xx)
+  if(linear.scale){
+    yy[which(yy==0)] = NA;
+    yy = log2(yy)
+  }
+  pairs(yy, lower.panel=NULL, upper.panel=panel.fitting, main = main)
+  
+}
+
 
 ##########################################
 # several common functions for normalizations 
@@ -141,6 +355,10 @@ calculate.sizeFactors.DESeq2 = function(expr_mat)
   return(sizeFactors(dds))
   
 }
+
+
+
+
 
 ########################################################
 ########################################################
@@ -204,6 +422,7 @@ if(Test.Seurat.workflow){
   DimPlot(object = pbmc_phate, reduction.use = 'phate', pt.size = 2.0)
   #DimPlot(object = pbmc_phate, reduction.use = 'pca')
 }
+
 
 ##################################################
 # test Hamberg's single-cell RNA seq analysis, clustering part
