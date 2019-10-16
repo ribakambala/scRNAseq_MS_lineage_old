12fc1eea434ca923e1eba4b65a619253b481b5f2
jingkui.wang <jingkui.wang@imp.ac.at>
2019-10-16 12:12:11 +0200

    add script for clustering function with all information, gene expression, facs info, timing, or even intronic signals

---
+++ scripts/Iterative_Clustering.R
@@ -0,0 +1,14 @@
+##########################################################################
+##########################################################################
+# Project: Aleks' single cell RNA-seq 
+# Script purpose: iterative clustering function
+# Usage example: 
+# Author: Jingkui Wang (jingkui.wang@imp.ac.at)
+# Date of creation: Wed Oct 16 11:38:07 2019
+##########################################################################
+##########################################################################
+
+find.timer.genes = function()
+{
+  
+}
