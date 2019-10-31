bdcbaa86d84ac278675539c27d86a213a2e758fd
jingkui.wang <jingkui.wang@imp.ac.at>
2019-10-31 13:30:32 +0100

    start to clean ustomized_clustering.R and restructure it

--- scripts/customized_clustering.R
+++ scripts/customized_clustering.R
@@ -34,7 +34,7 @@ cal_autocor_stationaryTest_sd = function(var, plot = FALSE)
   
 }
 
-prepare.HashimshonyLineageData = function()
+Test.timingEstimate.with.HashimshonyLineages = function()
 {
   require('openxlsx')
   lineages = read.xlsx(paste0(dataDir.Hashimsholy, "/Hashimshony_datasets_lineage_11timepoints.xlsx"), sheet = 1, startRow = 9, colNames = FALSE)
@@ -64,50 +64,22 @@ prepare.HashimshonyLineageData = function()
   rownames(test) = rownames(xx)
   test = log2(test + 2^-6)
   
-  estimate.timing(test[, 1], timers)  
+  estimate.timing(test[, 1], timers)
+  
+  
+  for(kk in c(1:55)){
+    # kk = 55
+    
+  }
+  
+  
   
 }
 
 find.timer.genes = function(Test.timer.genes.with.HashimshonyData = FALSE)
 {
   dataDir.Hashimsholy = '../data/Hashimsholy_et_al'
-  
-  
-  double.check.lineage.independent.genes = FALSE
-  if(double.check.lineage.independent.genes){
-    require('openxlsx')
-    lineages = read.xlsx(paste0(dataDir.Hashimsholy, "/Hashimshony_datasets_lineage_11timepoints.xlsx"), sheet = 1, startRow = 9, colNames = FALSE)
-    
-    names = lineages[c(1, 2), ]
-    xx = lineages[-c(1, 2), ]
-    
-    lineages.names = c('AB_T', 'MS_T', 'E_T', "C_T", 'P3_T')
-    timepoints = c(1:11)
-    colnames(xx) = c('ensEMBL.IDs', 'Wormbase',  as.vector(t(outer(lineages.names, timepoints, paste, sep="")))) 
-    
-    geneMapping = xx[, c(1, 2)] 
-    
-    #save(geneMapping, file = paste0(dataDir.Hashimsholy, "/geneNames_mapping_Wormbase.Rdata"))
-    
-    rownames(xx) = xx[, 1]
-    xx = xx[, -c(1, 2)]
-    kk = grep('T1$|T2', colnames(xx))
-    xx = xx[, -kk]
-    xx = as.matrix(xx)
-    ss = apply(xx, 1, sum)
-    jj = which(ss>2^5)
-    
-    xx = xx[jj, ]
-    
-    xx = log2(xx + 2^-8)
-    
-    yy = t(apply(xx, 1, cal_cor_sd_time))
-    
-    plot(yy[, 1], yy[, 3], cex = 0.5)
-    plot(yy[, 2], yy[, 4], cex = 0.5)
-  }else{
-    load( file = paste0(dataDir.Hashimsholy, "/geneNames_mapping_Wormbase.Rdata")) 
-  }
+  load( file = paste0(dataDir.Hashimsholy, "/geneNames_mapping_Wormbase.Rdata"))
   
   bulk = read.delim(file = paste0(dataDir.Hashimsholy, "/GSE50548_Whole_embryo_interval_timecourse.txt"), 
                     row.names = 1,  header = TRUE, sep = "\t")
@@ -145,36 +117,36 @@ find.timer.genes = function(Test.timer.genes.with.HashimshonyData = FALSE)
 }
 
 
-estimate.timing.with.timer.genes = function(mat, timers)
+estimate.timing.with.timer.genes = function(vec, timerGenes.ac =  0.6, timerGenes.sd = 2, loess.span = 1.5)
 {
-  for(kk in c(1:55)){
-    # kk = 55
-    vec = test[, kk]
-    corrs = rep(NA, length = ncol(timers))
-    names(corrs) = colnames(timers)
-    timepoints = gsub('mpfc_','',  colnames(timers))
-    timepoints[c(1:8)] = c('0', '5', '10', '20', '40', '50', '60', '70')
-    timepoints = as.numeric(timepoints)
-    
-    for(n in 1:ncol(timers)) {
-      #ii = which(vec > (-6) & timers[,n] > (-6))
-      ii = which(timers[,n] > (-6))
-      # ii = c(1:nrow(timers))
-      corrs[n] = cor(vec[ii], timers[ii,n], method = 'pearson')
-    }
-    predFun = loess(corrs ~ timepoints, span = 0.15)
-    
-    prediction = predict(predFun, timepoints, se = FALSE)
-    index = which(prediction==max(prediction))
-    estimate.timing = timepoints[index]
-    
-    plot(timepoints, corrs)
-    points(timepoints, prediction, type = 'l', lwd=1.5, col='darkblue')
-    points(timepoints[index], prediction[index], col = 'darkred', cex = 2.0, pch = 16)
-    
-    cat(colnames(test)[kk], " vs. ", estimate.timing, "min -- corr : ", prediction[index], "\n")
-    
+  # input: 
+  # vec -- a vector of gene expression with names of WBGeneID 
+  # 
+  
+  vec = test[, kk]
+  corrs = rep(NA, length = ncol(timers))
+  names(corrs) = colnames(timers)
+  timepoints = gsub('mpfc_','',  colnames(timers))
+  timepoints[c(1:8)] = c('0', '5', '10', '20', '40', '50', '60', '70')
+  timepoints = as.numeric(timepoints)
+  
+  for(n in 1:ncol(timers)) {
+    #ii = which(vec > (-6) & timers[,n] > (-6))
+    ii = which(timers[,n] > (-6))
+    # ii = c(1:nrow(timers))
+    corrs[n] = cor(vec[ii], timers[ii,n], method = 'pearson')
   }
+  predFun = loess(corrs ~ timepoints, span = 0.15)
+  
+  prediction = predict(predFun, timepoints, se = FALSE)
+  index = which(prediction==max(prediction))
+  estimate.timing = timepoints[index]
+  
+  plot(timepoints, corrs)
+  points(timepoints, prediction, type = 'l', lwd=1.5, col='darkblue')
+  points(timepoints[index], prediction[index], col = 'darkred', cex = 2.0, pch = 16)
+  
+  cat(colnames(test)[kk], " vs. ", estimate.timing, "min -- corr : ", prediction[index], "\n")
   
   return(estimate.timing)
   
