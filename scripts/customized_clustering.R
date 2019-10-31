f2a4ffc9db8dfbb8bc212e2b37ae2364624274ce
jingkui.wang <jingkui.wang@imp.ac.at>
2019-10-31 13:15:17 +0100

    save the first version of estimate timing function before reorganize it

--- scripts/customized_clustering.R
+++ scripts/customized_clustering.R
@@ -34,7 +34,7 @@ cal_autocor_stationaryTest_sd = function(var, plot = FALSE)
   
 }
 
-Test.timer.genes.with.HashimshonyLineageData = function(timers)
+prepare.HashimshonyLineageData = function()
 {
   require('openxlsx')
   lineages = read.xlsx(paste0(dataDir.Hashimsholy, "/Hashimshony_datasets_lineage_11timepoints.xlsx"), sheet = 1, startRow = 9, colNames = FALSE)
@@ -68,10 +68,85 @@ Test.timer.genes.with.HashimshonyLineageData = function(timers)
   
 }
 
-
-estimate.timing = function(vec, timers)
+find.timer.genes = function(Test.timer.genes.with.HashimshonyData = FALSE)
 {
+  dataDir.Hashimsholy = '../data/Hashimsholy_et_al'
   
+  
+  double.check.lineage.independent.genes = FALSE
+  if(double.check.lineage.independent.genes){
+    require('openxlsx')
+    lineages = read.xlsx(paste0(dataDir.Hashimsholy, "/Hashimshony_datasets_lineage_11timepoints.xlsx"), sheet = 1, startRow = 9, colNames = FALSE)
+    
+    names = lineages[c(1, 2), ]
+    xx = lineages[-c(1, 2), ]
+    
+    lineages.names = c('AB_T', 'MS_T', 'E_T', "C_T", 'P3_T')
+    timepoints = c(1:11)
+    colnames(xx) = c('ensEMBL.IDs', 'Wormbase',  as.vector(t(outer(lineages.names, timepoints, paste, sep="")))) 
+    
+    geneMapping = xx[, c(1, 2)] 
+    
+    #save(geneMapping, file = paste0(dataDir.Hashimsholy, "/geneNames_mapping_Wormbase.Rdata"))
+    
+    rownames(xx) = xx[, 1]
+    xx = xx[, -c(1, 2)]
+    kk = grep('T1$|T2', colnames(xx))
+    xx = xx[, -kk]
+    xx = as.matrix(xx)
+    ss = apply(xx, 1, sum)
+    jj = which(ss>2^5)
+    
+    xx = xx[jj, ]
+    
+    xx = log2(xx + 2^-8)
+    
+    yy = t(apply(xx, 1, cal_cor_sd_time))
+    
+    plot(yy[, 1], yy[, 3], cex = 0.5)
+    plot(yy[, 2], yy[, 4], cex = 0.5)
+  }else{
+    load( file = paste0(dataDir.Hashimsholy, "/geneNames_mapping_Wormbase.Rdata")) 
+  }
+  
+  bulk = read.delim(file = paste0(dataDir.Hashimsholy, "/GSE50548_Whole_embryo_interval_timecourse.txt"), 
+                    row.names = 1,  header = TRUE, sep = "\t")
+  
+  mm = match(rownames(bulk), geneMapping$ensEMBL.IDs)
+  rownames(bulk) = geneMapping$Wormbase[mm]
+  
+  bulk = as.matrix(bulk)
+  ss = apply(bulk, 1, sum)
+  kk = which(ss>2^5)
+  bulk = bulk[kk, ]
+  
+  bulk = log2(bulk + 2^-6)
+  
+  library(tseries)
+  
+  yy = t(apply(bulk, 1, cal_autocor_stationaryTest_sd))
+  
+  plot(yy[, c(1, 3)], cex = 0.5);
+  abline(v = 0.6, col='red')
+  abline(h = 1.5, col = 'red')
+  
+  yy = data.frame(yy)
+  sels = which(yy$ac.max>0.6 & yy$sd > 2)
+  
+  timers = bulk[sels, ]
+  
+  plot(timers[100,], type='b')
+  
+  if(Test.timer.genes.with.HashimshonyData){
+    Test.timer.genes.with.HashimshonyLineageData(timers)
+  }
+  
+  return(timers)
+}
+
+
+estimate.timing.with.timer.genes = function(mat, timers)
+{
   for(kk in c(1:55)){
     # kk = 55
     vec = test[, kk]
@@ -107,81 +182,7 @@ estimate.timing = function(vec, timers)
 
 
 
-find.timer.genes = function(Test.timer.genes.with.HashimshonyData = FALSE)
-{
-   dataDir.Hashimsholy = '../data/Hashimsholy_et_al'
-  
-   
-   double.check.lineage.independent.genes = FALSE
-   if(double.check.lineage.independent.genes){
-     require('openxlsx')
-     lineages = read.xlsx(paste0(dataDir.Hashimsholy, "/Hashimshony_datasets_lineage_11timepoints.xlsx"), sheet = 1, startRow = 9, colNames = FALSE)
-     
-     names = lineages[c(1, 2), ]
-     xx = lineages[-c(1, 2), ]
-     
-     lineages.names = c('AB_T', 'MS_T', 'E_T', "C_T", 'P3_T')
-     timepoints = c(1:11)
-     colnames(xx) = c('ensEMBL.IDs', 'Wormbase',  as.vector(t(outer(lineages.names, timepoints, paste, sep="")))) 
-     
-     geneMapping = xx[, c(1, 2)] 
-     
-     #save(geneMapping, file = paste0(dataDir.Hashimsholy, "/geneNames_mapping_Wormbase.Rdata"))
-     
-     rownames(xx) = xx[, 1]
-     xx = xx[, -c(1, 2)]
-     kk = grep('T1$|T2', colnames(xx))
-     xx = xx[, -kk]
-     xx = as.matrix(xx)
-     ss = apply(xx, 1, sum)
-     jj = which(ss>2^5)
-     
-     xx = xx[jj, ]
-     
-     xx = log2(xx + 2^-8)
-     
-     yy = t(apply(xx, 1, cal_cor_sd_time))
-     
-     plot(yy[, 1], yy[, 3], cex = 0.5)
-     plot(yy[, 2], yy[, 4], cex = 0.5)
-   }else{
-    load( file = paste0(dataDir.Hashimsholy, "/geneNames_mapping_Wormbase.Rdata")) 
-   }
-   
-   bulk = read.delim(file = paste0(dataDir.Hashimsholy, "/GSE50548_Whole_embryo_interval_timecourse.txt"), 
-                    row.names = 1,  header = TRUE, sep = "\t")
-   
-   mm = match(rownames(bulk), geneMapping$ensEMBL.IDs)
-   rownames(bulk) = geneMapping$Wormbase[mm]
-   
-   bulk = as.matrix(bulk)
-   ss = apply(bulk, 1, sum)
-   kk = which(ss>2^5)
-   bulk = bulk[kk, ]
-   
-   bulk = log2(bulk + 2^-6)
-   
-   library(tseries)
-   
-   yy = t(apply(bulk, 1, cal_autocor_stationaryTest_sd))
-   
-   plot(yy[, c(1, 3)], cex = 0.5);
-   abline(v = 0.6, col='red')
-   abline(h = 1.5, col = 'red')
-   
-   yy = data.frame(yy)
-   sels = which(yy$ac.max>0.6 & yy$sd > 2)
-   
-   timers = bulk[sels, ]
-   
-   plot(timers[100,], type='b')
-   
-   if(Test.timer.genes.with.HashimshonyData){
-     Test.timer.genes.with.HashimshonyLineageData(timers)
-   }
-   
-   return(timers)
-}
+
 
 
 
