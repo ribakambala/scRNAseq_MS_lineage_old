52ce46794a5522965fba00d1da5d031e37f7e674
jingkui.wang <jingkui.wang@imp.ac.at>
2019-11-05 14:37:24 +0100

    save the batch and clustering script after restructuring the code

--- scripts/scRNAseq_cellCycle_batch_clustering.R
+++ scripts/scRNAseq_cellCycle_batch_clustering.R
@@ -14,6 +14,7 @@
 ########################################################
 ########################################################
 Import.processed.data.from.Aleks = TRUE
+correct.cellCycle = FALSE
 
 path2AleksFolder = '/Volumes/groups/cochella/Aleks/bioinformatics/GitHub/scRNAseq_MS_lineage'
 version.DATA = 'scRNA_8613_full'
@@ -35,8 +36,6 @@ if(!dir.exists(RdataDir)){dir.create(RdataDir)}
 # we need to train the cells to identify the cell cycle phase
 # this could be more complicated than expected
 ##########################################
-correct.cellCycle = FALSE
-
 if(Import.processed.data.from.Aleks){
   load(file=paste0(RdataDirfromAleks, version.DATA, '_QCed_cells_genes_filtered_normalized_SCE.Rdata'))
 }else{
@@ -73,12 +72,14 @@ if(correct.cellCycle){
 }
 
 ##########################################
-# here to prepare the inputs for MNN in scran 
+##########################################
+# Prepare the data for batch correction (fastMNN from scran used here) and clustering 
 # Feature selection (HVGs) is performed
 # here we just use the scran trandVar()
 # further test to follow for other methods
 # (https://academic.oup.com/bib/advance-article/doi/10.1093/bib/bby011/4898116)
 # But many of them, e.g. BASics, Brennecke works better with spike-in 
+##########################################
 ##########################################
 library(scRNA.seq.funcs)
 library(scater)
@@ -95,7 +96,9 @@ facsInfo.Added = TRUE
 # 
 ##########################################
 if(Import.processed.data.from.Aleks){
+  
   load(file = paste0(RdataDirfromAleks, version.DATA, '_QCed_cells_genes_filtered_normalized_SCE_seuratCellCycleCorrectedv2_facsInfos.Rdata'))
+  save(sce, file=paste0(RdataDir, version.DATA, '_QCed_cells_genes_filtered_normalized_SCE_seuratCellCycleCorrectedv2_facsInfos.Rdata')) 
 }else{
   if(facsInfo.Added){
     load(file = paste0(RdataDir, version.DATA, '_QCed_cells_genes_filtered_normalized_SCE_seuratCellCycleCorrected_v2_facsInfos.Rdata'))
@@ -108,13 +111,13 @@ if(Import.processed.data.from.Aleks){
     save(sce, file=paste0(RdataDir, version.DATA, '_QCed_cells_genes_filtered_normalized_SCE_seuratCellCycleCorrectedv2_facsInfos.Rdata')) 
   }
 }
+
 # logcounts(sce) =  assay(sce, "logcounts_seurat_SG2MCorrected")
 table(sce$nb.cells)
 sce = sce[, which(sce$nb.cells == 1)]
 
 sce$FSC_log2 = 3/2*log2(sce$FSC)
 sce$BSC_log2 = 3/2*log2(sce$BSC)
-
 
 plotColData(sce,
             x = "FSC_log2",
@@ -123,6 +126,88 @@ plotColData(sce,
             point_size = 2
             
 )
+
+########################################################
+########################################################
+# Section : Clustering section by integrating various informations: 
+# gene expression, fac info, estimated timing and velocity 
+########################################################
+########################################################
+Test.timingEstimate.with.HashimshonyLineages = function()
+{
+  dataDir.Hashimsholy = '../data/Hashimsholy_et_al'
+  require('openxlsx')
+  lineages = read.xlsx(paste0(dataDir.Hashimsholy, "/Hashimshony_datasets_lineage_11timepoints.xlsx"), sheet = 1, startRow = 9, colNames = FALSE)
+  
+  names = lineages[c(1, 2), ]
+  xx = lineages[-c(1, 2), ]
+  
+  lineages.names = c('AB_T', 'MS_T', 'E_T', "C_T", 'P3_T')
+  timepoints = c(1:11)
+  colnames(xx) = c('ensEMBL.IDs', 'Wormbase',  as.vector(t(outer(lineages.names, timepoints, paste, sep="")))) 
+  
+  ## keep genes considered as timers
+  xx = xx[match(rownames(timers), xx[,2]), ]
+  rownames(xx) = xx[, 2]
+  
+  xx = xx[, -c(1, 2)]
+  test = mapply(xx, FUN=as.numeric)
+  rownames(test) = rownames(xx)
+  test = log2(test + 2^-6)
+  estimate.timing(test[, 1], timers)
+  
+  ## import the table downloaded from GEO
+  aa = read.table(paste0(dataDir.Hashimsholy, "/GSE50548_Blastomere_developmental_event_timecourse.txt"), header = TRUE)
+  load(file = paste0(dataDir.Hashimsholy, "/annotMapping_ensID_Wormbase_GeneName.Rdata"))
+  
+  names = geneMapping$Gene.name[match(rownames(aa), geneMapping$ensEMBL.IDs)]
+  jj = !is.na(names)
+  bb = aa[jj, ]
+  rownames(bb) = names[jj]
+  
+  test = mapply(bb, FUN=as.numeric)
+  rownames(test) = rownames(bb)
+  test = log2(test + 2^-6)
+  
+  
+  source('customized_clustering.R')
+  
+  timerGenes.pval = 0.001;
+  loess.span = 0.15;
+  use = 'lowFilter.both'
+  
+  pdfname = paste0("../results/clustering_combining_variousInfos/test_timing_estimation_Hashimshony_lineages.pdf")
+  pdf(pdfname, width=10, height = 6)
+  par(cex =0.7, mar = c(3,3,2,0.8)+0.1, mgp = c(1.6,0.5,0),las = 0, tcl = -0.3)
+  
+  estimation = c()
+  for(kk in c(1:55)){
+    # kk = 55
+    timing = estimate.timing.with.timer.genes(vec = test[,kk], PLOT.test = TRUE, timerGenes.pval=timerGenes.pval, use = use, 
+                                              loess.span = loess.span)
+    cat(colnames(test)[kk], " vs. ", timing, "min \n")
+    estimation = c(estimation, timing)
+  }
+  
+  experiments = rep(c(0, 20, 40, 60, 90, 110, 140, 180, 200, 300, 400), 5) + 5
+  
+  plot(experiments, estimation, type='n', main = paste0('timerGene.pval = ', signif(timerGenes.pval, d= 3), ' & loess.span = ', signif(loess.span, d=2)))
+  for(n in c(1:5)) {
+    index = seq(11*(n-1)+1, 11*n, by = 1)
+    points(experiments[index], estimation[index], pch = n, cex = 1.2, col = 'darkblue')
+  }
+  abline(0, 1, lwd=2.0, col='darkred')
+  abline(-20, 1, lwd=1.0, col='darkred', lty = 2)
+  abline(-40, 1, lwd=1.0, col='darkred', lty= 2)
+  abline(-60, 1, lwd=1.0, col='darkred', lty= 2)
+  legend('topleft', legend = c('AB', 'MS', 'E', 'C', 'P3'), pch = c(1:5), col = 'darkblue', bty = 'n')
+  
+  
+  dev.off()
+  
+  
+}
+
 
 ##########################################
 # Feature selection (select HGVs) for batch corrections
@@ -574,83 +659,3 @@ if(Select.Early.timePoints){
   
 }
 
-########################################################
-########################################################
-# Section : Clustering section by integrating various informations: 
-# gene expression, fac info, estimated timing and velocity 
-########################################################
-########################################################
-Test.timingEstimate.with.HashimshonyLineages = function()
-{
-  dataDir.Hashimsholy = '../data/Hashimsholy_et_al'
-  require('openxlsx')
-  lineages = read.xlsx(paste0(dataDir.Hashimsholy, "/Hashimshony_datasets_lineage_11timepoints.xlsx"), sheet = 1, startRow = 9, colNames = FALSE)
-  
-  names = lineages[c(1, 2), ]
-  xx = lineages[-c(1, 2), ]
-  
-  lineages.names = c('AB_T', 'MS_T', 'E_T', "C_T", 'P3_T')
-  timepoints = c(1:11)
-  colnames(xx) = c('ensEMBL.IDs', 'Wormbase',  as.vector(t(outer(lineages.names, timepoints, paste, sep="")))) 
-  
-  ## keep genes considered as timers
-  xx = xx[match(rownames(timers), xx[,2]), ]
-  rownames(xx) = xx[, 2]
-  
-  xx = xx[, -c(1, 2)]
-  test = mapply(xx, FUN=as.numeric)
-  rownames(test) = rownames(xx)
-  test = log2(test + 2^-6)
-  estimate.timing(test[, 1], timers)
-  
-  ## import the table downloaded from GEO
-  aa = read.table(paste0(dataDir.Hashimsholy, "/GSE50548_Blastomere_developmental_event_timecourse.txt"), header = TRUE)
-  load(file = paste0(dataDir.Hashimsholy, "/annotMapping_ensID_Wormbase_GeneName.Rdata"))
-  
-  names = geneMapping$Gene.name[match(rownames(aa), geneMapping$ensEMBL.IDs)]
-  jj = !is.na(names)
-  bb = aa[jj, ]
-  rownames(bb) = names[jj]
-
-  test = mapply(bb, FUN=as.numeric)
-  rownames(test) = rownames(bb)
-  test = log2(test + 2^-6)
- 
-  
-  source('customized_clustering.R')
-  
-  timerGenes.pval = 0.001;
-  loess.span = 0.15;
-  use = 'lowFilter.both'
-  
-  pdfname = paste0("../results/clustering_combining_variousInfos/test_timing_estimation_Hashimshony_lineages.pdf")
-  pdf(pdfname, width=10, height = 6)
-  par(cex =0.7, mar = c(3,3,2,0.8)+0.1, mgp = c(1.6,0.5,0),las = 0, tcl = -0.3)
-  
-  estimation = c()
-  for(kk in c(1:55)){
-    # kk = 55
-    timing = estimate.timing.with.timer.genes(vec = test[,kk], PLOT.test = TRUE, timerGenes.pval=timerGenes.pval, use = use, 
-                                              loess.span = loess.span)
-    cat(colnames(test)[kk], " vs. ", timing, "min \n")
-    estimation = c(estimation, timing)
-  }
-  
-  experiments = rep(c(0, 20, 40, 60, 90, 110, 140, 180, 200, 300, 400), 5) + 5
-  
-  plot(experiments, estimation, type='n', main = paste0('timerGene.pval = ', signif(timerGenes.pval, d= 3), ' & loess.span = ', signif(loess.span, d=2)))
-  for(n in c(1:5)) {
-    index = seq(11*(n-1)+1, 11*n, by = 1)
-    points(experiments[index], estimation[index], pch = n, cex = 1.2, col = 'darkblue')
-  }
-  abline(0, 1, lwd=2.0, col='darkred')
-  abline(-20, 1, lwd=1.0, col='darkred', lty = 2)
-  abline(-40, 1, lwd=1.0, col='darkred', lty= 2)
-  abline(-60, 1, lwd=1.0, col='darkred', lty= 2)
-  legend('topleft', legend = c('AB', 'MS', 'E', 'C', 'P3'), pch = c(1:5), col = 'darkblue', bty = 'n')
-  
-  
-  dev.off()
-  
-  
-}
